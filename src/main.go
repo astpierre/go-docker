418fe7184baf9d743a4e018c9f44f4598d488897
Andrew St. Pierre <andrew.stpierre@gmail.com>
2019-10-08 09:48:59 -0400

    ran go fmt over main file

--- src/main.go
+++ src/main.go
@@ -3,19 +3,18 @@ package main
 import (
 	"fmt"
 	"html"
-  "log"
-  "net/http"
+	"log"
+	"net/http"
 )
-
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-  })
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
 
-  http.HandleFunc("/bye", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Goodbye")
-  })
+	http.HandleFunc("/bye", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Goodbye")
+	})
 
-  log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }

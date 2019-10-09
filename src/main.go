0bcf81df47b0aee25f03ae1642fd52397ab7c12b
Andrew St. Pierre <andrew.stpierre@gmail.com>
2019-10-08 22:19:54 -0400

    hello world

--- src/main.go
+++ src/main.go
@@ -2,19 +2,8 @@ package main
 
 import (
 	"fmt"
-	"html"
-	"log"
-	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
-
-	http.HandleFunc("/bye", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Goodbye")
-	})
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("Hello, world.\n")
 }
